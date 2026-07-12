f3808ebcad2aae9480cb3c324a1bcb3afecdbb5a
MD SAIF ALAM <saifalam763155@gmail.com>
2026-07-12 14:22:33 +0530

    New code for mongoose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,11 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URL,{
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGODB_URL);
+
+mongoose.connection.on("connected", () => {
+    console.log("✅ MongoDB Connected");
+});
+
+mongoose.connection.on("error", (err) => {
+    console.error("❌ MongoDB Error:", err);
+});
